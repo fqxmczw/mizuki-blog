13884af5a036e0617e6f0e2b2b336dd7427a5b13
fqxmczw <124880355+fqxmczw@users.noreply.github.com>
2026-05-30 22:15:37 +0800

    Update devices.ts
    更新我的设备

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -16,22 +16,12 @@ export type DeviceCategory = Record<string, Device[]> & {
 export const devicesData: DeviceCategory = {
 	OnePlus: [
 		{
-			name: "OnePlus 13T",
-			image: "/images/device/oneplus13t.webp",
-			specs: "Gray / 16G + 1TB",
+			name: "OnePlus 13",
+			image: "/images/device/oneplus13.webp",
+			specs: "Gray / 16G + 512GB",
 			description:
-				"Flagship performance, Hasselblad imaging, 80W SuperVOOC.",
-			link: "https://www.oneplus.com/cn/13t",
-		},
-	],
-	Router: [
-		{
-			name: "GL-MT3000",
-			image: "/images/device/mt3000.webp",
-			specs: "1000Mbps / 2.5G",
-			description:
-				"Portable WiFi 6 router suitable for business trips and home use.",
-			link: "https://www.gl-inet.cn/products/gl-mt3000/",
+				"Flagship performance, Hasselblad imaging, 100W SuperVOOC.",
+			link: "https://www.oneplus.com/cn/13",
 		},
 	],
 };
