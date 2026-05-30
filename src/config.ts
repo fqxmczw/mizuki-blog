8f24e2b57941c50ac0c8974fee58145b945c4be8
fqxmczw <124880355+fqxmczw@users.noreply.github.com>
2026-05-30 20:58:20 +0800

    Update config.ts
    更正站点开始时间，填写番剧id

--- src/config.ts
+++ src/config.ts
@@ -27,7 +27,7 @@ export const siteConfig: SiteConfig = {
 	title: "Kongclのblog",
 	subtitle: "One demo website",
 	siteURL: "https://woblog.ccwu.cc/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-06-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+	siteStartDate: "2026-05-30", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	lang: SITE_LANG,
 
@@ -67,12 +67,12 @@ export const siteConfig: SiteConfig = {
 	},
 
 	bangumi: {
-		userId: "your-bangumi-id", // 在此处设置你的Bangumi用户ID，可以设置为 "sai" 测试
+		userId: "1253145", // 在此处设置你的Bangumi用户ID，可以设置为 "sai" 测试
 		fetchOnDev: false, // 是否在开发环境下获取 Bangumi 数据（默认 false），获取前先执行 pnpm build 构建 json 文件
 	},
 
 	bilibili: {
-		vmid: "your-bilibili-vmid", // 在此处设置你的Bilibili用户ID (uid)，例如 "1129280784"
+		vmid: "1054456879", // 在此处设置你的Bilibili用户ID (uid)，例如 "1129280784"
 		fetchOnDev: false, // 是否在开发环境下获取 Bilibili 数据（默认 false）
 		coverMirror: "", // 封面图片镜像源（可选，如果需要使用镜像源，例如 "https://images.weserv.nl/?url="）
 		useWebp: true, // 是否使用WebP格式（默认 true）
